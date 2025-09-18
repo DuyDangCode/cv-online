514e2dd0dc36f4969e40c20d553c745abca502ad
Thành Duy <21520774@gm.uit.edu.vn>
2025-09-18 12:22:48 +0700

    add data

---
+++ src/data/experience.js
@@ -0,0 +1,13 @@
+export const EXPERIENCES = [
+  {
+    position: 'Full-Stack Web Developer',
+    company: 'Dong Nai Information Technology Center',
+    duration: 'Jun 2024 - Nov 2024',
+    description:
+      'Dong Nai Information Technology Center is a provincial IT center focusing on software solutions, IT training, and digital transformation for local government and businesses.',
+    responsibilities: [
+      'Developed a web app to manage domain status',
+      'Detected compromised domains using search engine technology',
+    ],
+  },
+]
---
+++ src/data/skills.js
@@ -0,0 +1,22 @@
+export const SKILLS = {
+  backend: [
+    'Java',
+    'Spring Boot (Spring MVC, JPA, Security, Validation)',
+    'JavaScript',
+    'Node.js (Express)',
+    'SQL (MySQL)',
+    'NoSQL (MongoDB, Redis)',
+    'RESTful API',
+  ],
+  frontend: [
+    'TypeScript',
+    'React.js',
+    'Next.js',
+    'Tailwind CSS',
+    'Mantine UI',
+    'React Query',
+    'Leaflet.js',
+  ],
+  devops: ['Docker', 'AWS (EC2, IAM)', 'Cloudinary'],
+  nonTechnical: ['English: TOEIC L&R 660, S&W 260'],
+}
